c66eaa8bff6b7cb1dc963f7752063fe402558143
hongsonjs <hongson.js@gmail.com>
2024-07-26 23:04:50 +0700

    Add response object

---
+++ QuanLyKhoaHoc.Application/Payloads/Responses/ResponseObject.cs
@@ -0,0 +1,17 @@
+﻿namespace QuanLyKhoaHoc.Application.Payloads.Responses
+{
+    public class ResponseObject<T>
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+        public ResponseObject() { }
+
+        public ResponseObject(int status, string message, T? data)
+        {
+            Status = status;
+            Message = message;
+            Data = data;
+        }
+    }
+}
