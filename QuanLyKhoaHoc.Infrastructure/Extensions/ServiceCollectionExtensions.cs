c7ad3a3c5481d965361184df6af44bfaa4a75d21
hongsonjs <hongson.js@gmail.com>
2024-07-26 17:41:53 +0700

    Add connect sqlserver to extension

--- QuanLyKhoaHoc.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ QuanLyKhoaHoc.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using QuanLyKhoaHoc.Infrastructure.DataContexts;
 
 namespace QuanLyKhoaHoc.Infrastructure.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddInfrastructure(this IServiceCollection services) {
-            services.AddDbContext<AppDBContext>();
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDBContext>(options => options.UseSqlServer(connectionString));
         }
     }
 }
