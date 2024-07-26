23cc11987f01c48359b179f7fd57f3620481763c
hongsonjs <hongson.js@gmail.com>
2024-07-26 22:56:25 +0700

    Add validate input email and phone number

---
+++ QuanLyKhoaHoc.Domain/Validations/ValidateInput.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace QuanLyKhoaHoc.Domain.Validations
+{
+    public class ValidateInput
+    {
+        public static bool IsValidEmail(string email)
+        {
+            var emailAttribute = new EmailAddressAttribute();
+            return emailAttribute.IsValid(email);
+        }
+
+        public static bool IsValidPhoneNumber(string phoneNumber) {
+            string pattern = @"(?:\+84|0084|0)[235789][0-9]{1,2}[0-9]{7}(?:[^\d]+|$)";
+            return Regex.IsMatch(phoneNumber, pattern);
+        }
+    }
+}
