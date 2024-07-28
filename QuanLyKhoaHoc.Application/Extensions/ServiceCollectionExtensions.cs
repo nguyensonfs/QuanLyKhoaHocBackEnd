295dc1af14e4c3e2cf44c2c303dd4991b27c5675
hongsonjs <hongson.js@gmail.com>
2024-07-28 23:27:14 +0700

    Add Application extensions

---
+++ QuanLyKhoaHoc.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using QuanLyKhoaHoc.Application.ImplementServices;
+using QuanLyKhoaHoc.Application.InterfaceServices;
+using QuanLyKhoaHoc.Application.Payloads.Mappers;
+
+namespace QuanLyKhoaHoc.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<LoaiKhoaHocConverter>();
+            services.AddScoped<ILoaiKhoaHocService, LoaiKhoaHocService>();
+        }
+    }
+}
