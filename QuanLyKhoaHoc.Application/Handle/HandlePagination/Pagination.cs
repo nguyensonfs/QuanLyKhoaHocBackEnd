6df855321b03026f26219d5cd435362c59bb8503
hongsonjs <hongson.js@gmail.com>
2024-07-29 07:13:30 +0700

    Feat: HandlePagination

---
+++ QuanLyKhoaHoc.Application/Handle/HandlePagination/Pagination.cs
@@ -0,0 +1,15 @@
+﻿namespace QuanLyKhoaHoc.Application.Handle.HandlePagination
+{
+    public class Pagination
+    {
+        public static PageResult<T> GetPagedData<T>(IQueryable<T> data, int pageSize, int pageNumber)
+        {
+            int totalItems = data.Count();
+            int totalPages = pageSize == -1 ? 1 : (int)Math.Ceiling((decimal)totalItems / pageSize);
+
+            var pagedData = pageSize == -1 ? data : data.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+
+            return new PageResult<T>(pagedData, totalItems, totalPages, pageNumber, pageSize);
+        }
+    }
+}
