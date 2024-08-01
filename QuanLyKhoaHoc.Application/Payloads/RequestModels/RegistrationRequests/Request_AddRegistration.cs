143b2848d945a32751b79ebbd92cd5f3cad3ec47
hongsonjs <hongson.js@gmail.com>
2024-08-01 12:02:54 +0700

    Update: DangKyHoc Features

---
+++ QuanLyKhoaHoc.Application/Payloads/RequestModels/RegistrationRequests/Request_AddRegistration.cs
@@ -0,0 +1,18 @@
+﻿using QuanLyKhoaHoc.Application.Payloads.RequestModels.StudentRequests;
+
+namespace QuanLyKhoaHoc.Application.Payloads.RequestModels.RegistrationRequests
+{
+    public class Request_AddRegistration
+    {
+
+        public int KhoaHocID { get; set; }
+
+        public int HocVienID { get; set; }
+
+        public int TinhTrangHocID { get; set; }
+
+        public int TaiKhoanID { get; set; }
+
+        public Request_AddStudent Student { get; set; }
+    }
+}
---
+++ QuanLyKhoaHoc.Application/Payloads/ResponseModels/DataRegistration/DataResponeRegistration.cs
@@ -0,0 +1,21 @@
+﻿namespace QuanLyKhoaHoc.Application.Payloads.ResponseModels.DataRegistration
+{
+    public class DataResponeRegistration
+    {
+        public int DangKyHocID { get; set; }
+
+        public int KhoaHocID { get; set; }
+
+        public int HocVienID { get; set; }
+
+        public DateTime NgayDangKy { get; set; }
+
+        public DateTime NgayBatDau { get; set; }
+
+        public DateTime NgayKetThuc { get; set; }
+
+        public int TinhTrangHocID { get; set; }
+
+        public int TaiKhoanID { get; set; }
+    }
+}
