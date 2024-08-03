7d03c9359a0d3cfa86ecc1f843fe5768e8a59ad2
hongsonjs <hongson.js@gmail.com>
2024-08-03 17:52:15 +0700

    fix(IStudentService): refactor code

--- QuanLyKhoaHoc.Application/InterfaceServices/IStudentService.cs
+++ QuanLyKhoaHoc.Application/InterfaceServices/IStudentService.cs
@@ -7,10 +7,10 @@ namespace QuanLyKhoaHoc.Application.InterfaceServices
 {
     public interface IStudentService
     {
-        Task<ResponseObject<DataResponseStudent>> CreateSudent(Request_CreateStudent request);
-        Task<ResponseObject<DataResponseStudent>> UpdateSudent(int studentId,Request_UpdateStudent request);
         Task<PageResult<DataResponseStudent>> GetAlls(int pageSize, int pageNumber);
         Task<PageResult<DataResponseStudent>> SearchPagedStudents(string keyword, int pageNumber, int pageSize);
+        Task<ResponseObject<DataResponseStudent>> CreateSudent(Request_CreateStudent request);
+        Task<ResponseObject<DataResponseStudent>> UpdateSudent(int studentId,Request_UpdateStudent request);
         Task<string> DeleteStudent(int studentId);
     }
 }
